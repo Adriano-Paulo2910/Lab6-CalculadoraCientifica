99cabc11e7021d665a340e4a9e1dfc5ca52ac0ba
Luyandra_Branco <94245234+LuyandraBranco@users.noreply.github.com>
2023-06-10 13:52:31 +0100

    js/Calculadora: função square adicionada

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -1,60 +1,66 @@
 var historyList = document.getElementById('history-list');
 
-    function clearResult() {
-      document.getElementById('result').value = '';
-    }
+function clearResult() {
+  document.getElementById('result').value = '';
+}
 
-    function backspace() {
-      var currentResult = document.getElementById('result').value;
-      document.getElementById('result').value = currentResult.slice(0, -1);
-    }
+function backspace() {
+  var currentResult = document.getElementById('result').value;
+  document.getElementById('result').value = currentResult.slice(0, -1);
+}
 
-    function append(value) {
-      document.getElementById('result').value += value;
-    }
+function append(value) {
+  document.getElementById('result').value += value;
+}
 
-    function calculate() {
-      var expression = document.getElementById('result').value;
-      var result = eval(expression);
+function calculate() {
+  var expression = document.getElementById('result').value;
+  var result = eval(expression);
 
-      document.getElementById('result').value = result;
+  document.getElementById('result').value = result;
 
-      var historyItem = document.createElement('li');
-      historyItem.innerText = expression + ' = ' + result;
-      historyList.appendChild(historyItem);
-    }
+  var historyItem = document.createElement('li');
+  historyItem.innerText = expression + ' = ' + result;
+  historyList.appendChild(historyItem);
+}
 
-    function addToMemory() {
-      var result = document.getElementById('result').value;
-      localStorage.setItem('memory', result);
-    }
+function addToMemory() {
+  var result = document.getElementById('result').value;
+  localStorage.setItem('memory', result);
+}
 
-    function recallMemory() {
-      var memory = localStorage.getItem('memory');
-      if (memory !== null) {
-        document.getElementById('result').value += memory;
-      }
-    }
+function recallMemory() {
+  var memory = localStorage.getItem('memory');
+  if (memory !== null) {
+    document.getElementById('result').value += memory;
+  }
+}
 
-    function clearMemory() {
-      localStorage.removeItem('memory');
-    }
+function clearMemory() {
+  localStorage.removeItem('memory');
+}
 
-    function clearHistory() {
-        var historyList = document.getElementById('history-list');
-        historyList.innerHTML = '';
-    }
+function clearHistory() {
+  var historyList = document.getElementById('history-list');
+  historyList.innerHTML = '';
+}
 
-    function changeBackgroundColor() {
-      var colorSelect = document.getElementById('color-select');
-      var selectedColor = colorSelect.value;
-      var calculator = document.querySelector('.calculator');
-      calculator.style.backgroundColor = selectedColor;
-    }
+function changeBackgroundColor() {
+  var colorSelect = document.getElementById('color-select');
+  var selectedColor = colorSelect.value;
+  var calculator = document.querySelector('.calculator');
+  calculator.style.backgroundColor = selectedColor;
+}
 
-    function changeLanguage() {
-      var languageSelect = document.getElementById('language-select');
-      var selectedLanguage = languageSelect.value;
-      /**/
-      alert('Selected language: ' + selectedLanguage);
-    }
+function changeLanguage() {
+  var languageSelect = document.getElementById('language-select');
+  var selectedLanguage = languageSelect.value;
+  /**/
+  alert('Selected language: ' + selectedLanguage);
+}
+
+// Função para calcular o quadrado de um número
+function square() {
+  var result = document.getElementById('result').value;
+  document.getElementById('result').value = Math.pow(eval(result), 2);
+}

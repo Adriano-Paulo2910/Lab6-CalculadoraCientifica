b91a83ea3af67f74cbe0e79034ad0f27462f7804
Luyandra_Branco <94245234+LuyandraBranco@users.noreply.github.com>
2023-06-10 13:58:53 +0100

    js/Calculadora: função calculate modificada

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -13,15 +13,18 @@ function append(value) {
   document.getElementById('result').value += value;
 }
 
+// Função para calcular o resultado da expressão
 function calculate() {
   var expression = document.getElementById('result').value;
-  var result = eval(expression);
+  var result;
+
+  try {
+    result = eval(expression);
+  } catch (error) {
+    result = 'Erro';
+  }
 
   document.getElementById('result').value = result;
-
-  var historyItem = document.createElement('li');
-  historyItem.innerText = expression + ' = ' + result;
-  historyList.appendChild(historyItem);
 }
 
 function addToMemory() {
@@ -63,4 +66,10 @@ function changeLanguage() {
 function square() {
   var result = document.getElementById('result').value;
   document.getElementById('result').value = Math.pow(eval(result), 2);
+}
+
+// Função para calcular o inverso de um número
+function reciprocal() {
+  var result = document.getElementById('result').value;
+  document.getElementById('result').value = 1 / eval(result);
 }

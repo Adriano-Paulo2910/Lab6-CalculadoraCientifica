f02f4197dff8d3c9657eae80e40c40db00bb3673
Anderson <rafael.pires.752@gmail.com>
2023-06-11 23:32:34 +0100

    Idioma, historico, butoes de idioma e download

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -18,12 +18,36 @@ function calculate() {
   addToHistory(expression + ' = ' + result);
 }
 
+
+
 // Função para adicionar o resultado atual ao histórico
 function addToHistory(expression) {
   var li = document.createElement('li');
   li.appendChild(document.createTextNode(expression));
   document.getElementById('history-list').appendChild(li);
 }
+
+// Função para fazer download do histórico
+function downloadHistory() {
+  var historyList = document.getElementById('history-list');
+  var historyItems = historyList.getElementsByTagName('li');
+  
+  var content = '';
+  for (var i = 0; i < historyItems.length; i++) {
+    content += historyItems[i].textContent + '\n';
+  }
+  
+  var blob = new Blob([content], { type: 'text/plain' });
+  var url = URL.createObjectURL(blob);
+  
+  var downloadLink = document.createElement('a');
+  downloadLink.href = url;
+  downloadLink.download = 'historico.txt';
+  downloadLink.click();
+}
+
+// Adicionar evento de clique para o botão "Download Histórico"
+document.getElementById('btn-download').addEventListener('click', downloadHistory);
 
 // Função para limpar o histórico
 function clearHistory() {
@@ -164,3 +188,130 @@ function openConversionPage(option) {
     window.open('index.html', '_blank');
   }
 }
+
+//inicio dos idiomas 
+const langEl = document.querySelector('.langWrap');
+const link = document.querySelectorAll('a');
+const h3El = document.querySelector('.h3');
+const hisEl = document.querySelector('.his');
+const labelEl = document.querySelector('.label');
+const ccEl = document.querySelector('.cc');
+const vcEl = document.querySelector('.vc');
+const rcEl = document.querySelector('.rc');
+const azEl = document.querySelector('.az');
+const ptEl = document.querySelector('.pt');
+
+const caEl = document.querySelector('.ca');
+const coEl = document.querySelector('.co');
+const teEl = document.querySelector('.te');
+const maEl = document.querySelector('.ma');
+
+
+link.forEach(el =>{
+  el.addEventListener('click', () =>  {
+    langEl.querySelector('.active').classList.remove('active');
+    el.classList.add('active');
+
+    const attr = el.getAttribute('language');
+
+    h3El.textContent = data[attr].h3;
+    hisEl.textContent = data[attr].his;
+    labelEl.textContent = data[attr].label;
+    ccEl.textContent = data[attr].cc;
+    vcEl.textContent = data[attr].vc;
+    rcEl.textContent = data[attr].rc;
+    azEl.textContent = data[attr].az;
+    ptEl.textContent = data[attr].pt;
+
+    caEl.textContent = data[attr].ca;
+    coEl.textContent = data[attr].co;
+    teEl.textContent = data[attr].te;
+    maEl.textContent = data[attr].ma;
+
+  })
+});
+
+//Idiomas da calculadora
+var data ={
+  "portuguese":{
+    "h3" : "Histórico",
+    "his": "Limpar histórico",
+    "label": "Fundo",
+    "cc": "Cinzento claro",
+    "vc": "Verde claro",
+    "rc": "Rosa claro",
+    "az": "Azul",
+    "pt": "Preto",
+    "ca": "Calculadora Científica",
+    "co": "Comprimento",
+    "te": "Temperatura",
+    "ma": "Massa"
+  },"english":{
+    "h3" : "History",
+    "his": "Clear history",
+    "label": "Background",
+    "cc": "Light grey",
+    "vc": "Light green",
+    "rc": "Light pink",
+    "az": "Blue",
+    "pt": "Black",
+    "ca": "Scientific Calculator",
+    "co": "Length",
+    "te": "Temperature",
+    "ma": "Mass"
+  },"french":{
+    "h3" : "Historique",
+    "his": "Effacer l'historique",
+    "label": "Arrière-plan",
+    "cc": "Gris clair",
+    "vc": "Vert clair",
+    "rc": "Rose clair",
+    "az": "Bleu",
+    "pt": "Noir",
+    "ca": "Calculatrice Scientifique",
+    "co": "Longueur",
+    "te": "Température",
+    "ma": "Masse"
+  },"japanese":{
+    "h3" : "ヒストリコ",
+    "his": "履歴をクリア",
+    "label": "バックグラウンド",
+    "cc": "ライトグレー",
+    "vc": "ライトグリーン",
+    "rc": "ライトピンク",
+    "az": "青",
+    "pt": "黒",
+    "ca": "関数電卓",
+    "co": "長さ",
+    "te": "温度",
+    "ma": "質量"
+  },"chinese":{
+    "h3" : "历史",
+    "his": "清除历史记录",
+    "label": "背景",
+    "cc": "浅灰色",
+    "vc": "蓝色的",
+    "rc": "黑色的",
+    "az": "蓝色的",
+    "pt": "黑色的",
+    "ca": "科学计算器",
+    "co": "长度",
+    "te": "温度",
+    "ma": "质量"
+  }
+
+}
+
+// Função para verificar se a tecla pressionada é o Enter
+function verificarTecla(event) {
+  if (event.keyCode === 13) {
+    calculate();
+  }
+}
+
+// Obtém o elemento do campo de texto
+var inputElement = document.getElementById("result");
+
+// Adiciona o evento de tecla pressionada ao campo de texto
+inputElement.addEventListener("keydown", verificarTecla);
+
